64679f57515f734384b3741f9a19d615b96aee9f
Dagzz <thibaut.iacyber@gmail.com>
2024-11-22 14:18:03 +0100

    Mockups

---
+++ src/model/model.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title) {
+        self.title = title
+        self.author = ""
+        self.year_published = 0
+        self.pages = 0
+        self.genre = ""
+    }
+    
